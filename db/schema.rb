a34983b0fff7a28220f6f315a7988490d9214091
jayjay-a <agno.julius@gmail.com>
2018-03-11 20:39:55 -0500

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 
-ActiveRecord::Schema.define(version: 20180310035504) do
 ActiveRecord::Schema.define(version: 20180310042347) do
 
   # These are extensions that must be enabled in order to support this database
