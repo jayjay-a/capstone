b25dc70239891f28e5a2b011777d3c8a4c0d723c
julius agno <agno.julius@gmail.com>
2018-03-13 15:36:59 -0500

    create task nested in jobs in projects

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,7 +1,9 @@
 class Job < ApplicationRecord
-  has_many :job_notes
-  has_many :tasks
+  has_many :job_notes, dependent: :destroy
+  has_many :tasks, dependent: :destroy
   belongs_to :project, optional: true
   belongs_to :job_type
   belongs_to :job_status
+
+  accepts_nested_attributes_for :tasks, allow_destroy: true, reject_if: :all_blank
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   has_many :task_notes
   has_many :assignments
-  belongs_to :task_status
+  belongs_to :task_status, optional: true
   belongs_to :job
 end
