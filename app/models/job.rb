f11f8b48d46caf4949f9a85fdcd729270faefe08
julius agno <agno.julius@gmail.com>
2018-03-13 00:18:10 -0500

    add job to project

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,7 +1,7 @@
 class Job < ApplicationRecord
   has_many :job_notes
   has_many :tasks
-  belongs_to :project
+  belongs_to :project, optional: true
   belongs_to :job_type
   belongs_to :job_status
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,12 @@
 class Project < ApplicationRecord
-    has_many :jobs
-    has_many :project_notes
+    has_many :jobs, dependent: :destroy
+    has_many :project_notes, dependent: :destroy
     has_many :material_lists
     has_many :rental_lists
     belongs_to :customer
     belongs_to :project_status
     belongs_to :project_type
+
+    #cocoon
+    accepts_nested_attributes_for :jobs, allow_destroy: true, reject_if: :all_blank
 end
