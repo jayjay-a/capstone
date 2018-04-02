e12df331b2c28798689542f5bc4640fef9905104
julius agno <agno.julius@gmail.com>
2018-04-01 23:07:52 -0500

    report controller cleanup

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,6 +3,7 @@ class ReportsController < ApplicationController
 
   end
 
+  #terminated contractors
   def report1
     sql = "SELECT sub.subcontractor_name FROM Subcontractors sub
            JOIN Subcontractor_statuses subst ON sub.subcontractor_status_id = subst.subcontractor_status_id
@@ -15,13 +16,15 @@ class ReportsController < ApplicationController
     end
   end
 
+  #projects with bid greater than x dollars
   def report2
     sql = "SELECT proj.project_id, cust.customer_name, cust.customer_branch, projty.project_type_description, projstat.project_status_description
-FROM Projects proj
-JOIN Customers cust ON proj.customer_id = cust.customer_id
-JOIN Project_types projty ON proj.project_type_id = projty.project_type_id
-JOIN Project_statuses projstat on proj.project_status_id = projstat.project_status_id
-WHERE proj.bid_amount > 100;"
+           FROM Projects proj
+           JOIN Customers cust ON proj.customer_id = cust.customer_id
+           JOIN Project_types projty ON proj.project_type_id = projty.project_type_id
+           JOIN Project_statuses projstat on proj.project_status_id = projstat.project_status_id
+           WHERE proj.bid_amount > 100;"
+
     @great_bids = ActiveRecord::Base.connection.execute(sql)
     respond_to do |format|
       format.html
@@ -30,20 +33,21 @@ WHERE proj.bid_amount > 100;"
     end
   end
 
+  #completed projects
   def report3
-    sql = "Select
-projects.project_id,
-Customers.customer_name,
-project_statuses.project_status_description,
-projects.project_type_id,
-projects.project_start_date,
-projects.project_end_date,
-projects.bid_submit_Date
-From projects
-INNER JOIN Project_statuses ON Projects.project_status_id=Project_statuses.project_Status_id
-INNER JOIN Customers ON Customers.Customer_id = Projects.Customer_id
-Where project_statuses.project_status_description = 'project completed';"
-# Anil's SQL Report
+    sql = "SELECT
+           projects.project_id,
+           Customers.customer_name,
+           project_statuses.project_status_description,
+           projects.project_type_id,
+           projects.project_start_date,
+           projects.project_end_date,
+           projects.bid_submit_Date
+           FROM projects
+           INNER JOIN Project_statuses ON Projects.project_status_id=Project_statuses.project_Status_id
+           INNER JOIN Customers ON Customers.Customer_id = Projects.Customer_id
+           FROM project_statuses.project_status_description = 'Project Completed';"
+
     @complete_projects = ActiveRecord::Base.connection.execute(sql)
     respond_to do |format|
       format.html
@@ -52,65 +56,68 @@ Where project_statuses.project_status_description = 'project completed';"
     end
   end
 
+  #show active projects
   def report4
-    sql = "Select
-projects.project_id,
-Customers.customer_name,
-project_statuses.project_status_description,
-projects.project_type_id,
-projects.project_start_date,
-projects.project_end_date,
-projects.bid_submit_Date
-From projects
-INNER JOIN Project_statuses ON Projects.project_status_id=Project_statuses.project_Status_id
-INNER JOIN Customers ON Customers.Customer_id = Projects.Customer_id
-Where project_statuses.project_status_description = 'project ongoing'
-"
-    # Anil's SQL Report
+    sql = "SELECT
+           projects.project_id,
+           Customers.customer_name,
+           project_statuses.project_status_description,
+           projects.project_type_id,
+           projects.project_start_date,
+           projects.project_end_date,
+           projects.bid_submit_Date
+           FROM projects
+           INNER JOIN Project_statuses ON Projects.project_status_id=Project_statuses.project_Status_id
+           INNER JOIN Customers ON Customers.Customer_id = Projects.Customer_id
+           WHERE project_statuses.project_status_description = 'Project Active';"
+
     @active_projects = ActiveRecord::Base.connection.execute(sql)
     respond_to do |format|
       format.html
       format.xlsx
       format.pdf
     end
-    end
+  end
+
+  #awaiting bid response -anil
   def report5
-    sql = "Select
-projects.project_id,
-Customers.customer_name,
-project_statuses.project_status_description,
-projects.project_type_id,
-projects.project_start_date,
-projects.project_end_date,
-projects.bid_submit_Date
-From projects
-INNER JOIN Project_statuses ON Projects.project_status_id=Project_statuses.project_Status_id
-INNER JOIN Customers ON Customers.Customer_id = Projects.Customer_id
-Where project_statuses.project_status_description = 'Awaiting Bid Response';
-"
+    sql = "SELECT
+           projects.project_id,
+           Customers.customer_name,
+           project_statuses.project_status_description,
+           projects.project_type_id,
+           projects.project_start_date,
+           projects.project_end_date,
+           projects.bid_submit_Date
+           From projects
+           INNER JOIN Project_statuses ON Projects.project_status_id=Project_statuses.project_Status_id
+           INNER JOIN Customers ON Customers.Customer_id = Projects.Customer_id
+           Where project_statuses.project_status_description = 'Awaiting Bid Response';"
+
     @waiting_bid = ActiveRecord::Base.connection.execute(sql)
     respond_to do |format|
       format.html
       format.xlsx
       format.pdf
     end
-    #Anil's SQL Report
   end
+
+  #notes today -anil
   def report6
-    sql= "Select
-    projects.project_id,
-        Customers.customer_name,
-        project_statuses.project_status_description,
-        projects.bid_submit_date,
-        projects.project_start_date,
-        projects.project_end_date,
-        project_notes.project_notes,
-        project_notes.project_note_date
-    From projects
-    INNER JOIN Project_statuses ON Projects.project_status_id=Project_statuses.project_Status_id
-    INNER JOIN Customers ON Customers.Customer_id = Projects.Customer_id
-    INNER JOIN Project_notes ON Project_notes.Project_id = Projects.Project_id
-    Where project_notes.project_note_date = current_date::date;"
+    sql = "SELECT
+           projects.project_id,
+           Customers.customer_name,
+           project_statuses.project_status_description,
+           projects.bid_submit_date,
+           projects.project_start_date,
+           projects.project_end_date,
+           project_notes.project_notes,
+           project_notes.project_note_date
+           From projects
+           INNER JOIN Project_statuses ON Projects.project_status_id=Project_statuses.project_Status_id
+           INNER JOIN Customers ON Customers.Customer_id = Projects.Customer_id
+           INNER JOIN Project_notes ON Project_notes.Project_id = Projects.Project_id
+           Where project_notes.project_note_date = current_date::date;"
 
     @notes_today = ActiveRecord::Base.connection.execute(sql)
     respond_to do |format|
@@ -118,22 +125,22 @@ Where project_statuses.project_status_description = 'Awaiting Bid Response';
       format.xlsx
       format.pdf
     end
-    #Anil's SQL Report
   end
-  def report7
-    sql = "Select
-    projects.project_id,
-        Customers.customer_name,
-        project_statuses.project_status_description,
-        projects.project_type_id,
-        projects.project_start_date,
-        projects.project_end_date,
-        projects.bid_submit_Date
-    From projects
-    INNER JOIN Project_statuses ON Projects.project_status_id=Project_statuses.project_Status_id
-    INNER JOIN Customers ON Customers.Customer_id = Projects.Customer_id
-    Where project_statuses.project_status_description = 'bid denied';"
 
+  #show denied bids - anil
+  def report7
+    sql = "SELECT
+           projects.project_id,
+           Customers.customer_name,
+           project_statuses.project_status_description,
+           projects.project_type_id,
+           projects.project_start_date,
+           projects.project_end_date,
+           projects.bid_submit_Date
+           FROM projects
+           INNER JOIN Project_statuses ON Projects.project_status_id=Project_statuses.project_Status_id
+           INNER JOIN Customers ON Customers.Customer_id = Projects.Customer_id
+           WHERE project_statuses.project_status_description = 'Bid Denied';"
 
     @bid_denied = ActiveRecord::Base.connection.execute(sql)
     respond_to do |format|
@@ -141,22 +148,21 @@ Where project_statuses.project_status_description = 'Awaiting Bid Response';
       format.xlsx
       format.pdf
     end
-    #Anil's SQL Report
   end
+
+  #show bids debued - anil
   def report8
     sql = "SELECT
-    material_lists.project_id,
-        customers.customer_name,
-        customers.customer_branch,
-        materials.material_description,
-        material_lists.quantity
-
-    FROM material_lists
-    INNER JOIN materials on materials.material_id = material_lists.material_id
-    INNER JOIN projects on projects.project_id = material_lists.project_id
-    INNER JOIN customers on customers.customer_id = projects.customer_id
-
-    WHERE projects.project_id = 6;"
+           material_lists.project_id,
+           customers.customer_name,
+           customers.customer_branch,
+           materials.material_description,
+           material_lists.quantity
+           FROM material_lists
+           INNER JOIN materials on materials.material_id = material_lists.material_id
+           INNER JOIN projects on projects.project_id = material_lists.project_id
+           INNER JOIN customers on customers.customer_id = projects.customer_id
+           WHERE project_statuses.project_status_description = 'Bid Denied';"
 
     @specific_matlist = ActiveRecord::Base.connection.execute(sql)
     respond_to do |format|
@@ -164,14 +170,13 @@ Where project_statuses.project_status_description = 'Awaiting Bid Response';
       format.xlsx
       format.pdf
     end
-    #Anil's SQL Report
-
   end
+
+  #show certain customer 
   def report9
     sql = "SELECT c.customer_name, c.customer_branch, p.project_id, p.bid_amount
-FROM customers c
-JOIN projects p ON c.customer_id = p.customer_id;
-"
+           FROM customers c
+           JOIN projects p ON c.customer_id = p.customer_id;"
     @specific_custproj = ActiveRecord::Base.connection.execute(sql)
     respond_to do |format|
       format.html
@@ -179,13 +184,13 @@ JOIN projects p ON c.customer_id = p.customer_id;
       format.pdf
     end
   end
-  #Kristina's SQL Report
 
+  #show all projects and all job types - kristina
   def report10
     sql = "SELECT p.project_id, jt.job_type_description
-FROM projects p
-JOIN jobs j ON p.project_id = j.project_id
-JOIN job_types jt ON j.job_type_id = jt.job_type_id;"
+           FROM projects p
+           JOIN jobs j ON p.project_id = j.project_id
+           JOIN job_types jt ON j.job_type_id = jt.job_type_id;"
 
     @specific_jobproj = ActiveRecord::Base.connection.execute(sql)
     respond_to do |format|
@@ -194,14 +199,15 @@ JOIN job_types jt ON j.job_type_id = jt.job_type_id;"
       format.pdf
     end
   end
-  #Kristina's SQL Report
+
+  #show bid information from a project - kristina
   def report11
     sql = "SELECT PROJECT_ID, ps.PROJECT_STATUS_DESCRIPTION,
-BID_SUBMIT_DATE, BID_AMOUNT, BID_MATERIAL_COST,
-BID_COST_OF_LABOR, BID_COST_OF_PERMITS,
-BID_EQUIPMENT_RENTAL, BID_FREIGHT
-FROM PROJECTS p
-JOIN PROJECT_STATUSES ps ON p.project_status_id = ps.project_status_id;"
+           BID_SUBMIT_DATE, BID_AMOUNT, BID_MATERIAL_COST,
+           BID_COST_OF_LABOR, BID_COST_OF_PERMITS,
+           BID_EQUIPMENT_RENTAL, BID_FREIGHT
+           FROM PROJECTS p
+           JOIN PROJECT_STATUSES ps ON p.project_status_id = ps.project_status_id;"
 
     @bidinfo_proj = ActiveRecord::Base.connection.execute(sql)
     respond_to do |format|
@@ -210,13 +216,14 @@ JOIN PROJECT_STATUSES ps ON p.project_status_id = ps.project_status_id;"
       format.pdf
     end
   end
-  #Kristina's SQL Report
+  
+  #show projects and their job types and their job tasks - kristina
   def report12
     sql = "SELECT p.project_id, jt.job_type_description, t.task_description
-FROM projects p
-JOIN jobs j ON p.project_id = j.project_id
-JOIN job_types jt ON j.job_type_id = jt.job_type_id
-JOIN tasks t ON j.job_id = t.job_id;"
+           FROM projects p
+           JOIN jobs j ON p.project_id = j.project_id
+           JOIN job_types jt ON j.job_type_id = jt.job_type_id
+           JOIN tasks t ON j.job_id = t.job_id;"
 
     @alltask_job = ActiveRecord::Base.connection.execute(sql)
     respond_to do |format|
@@ -225,13 +232,12 @@ JOIN tasks t ON j.job_id = t.job_id;"
       format.pdf
     end
   end
-  #Kristina's SQL Report
 
+  #show all projects and their project type
   def report13
     sql = "SELECT project_id, project_type_description
-FROM projects p
-JOIN project_types pt ON p.project_type_id = pt.project_type_id;
-"
+           FROM projects p
+           JOIN project_types pt ON p.project_type_id = pt.project_type_id;"
     @specific_projtype = ActiveRecord::Base.connection.execute(sql)
     respond_to do |format|
       format.html
