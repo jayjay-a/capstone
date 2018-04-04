25794a1f85e38210b750fc30f165bc75ca36b65e
jayjay-a <agno.julius@gmail.com>
2018-04-04 12:44:47 -0500

    Update material_list.rb

--- app/models/material_list.rb
+++ app/models/material_list.rb
@@ -6,7 +6,7 @@ class MaterialList < ApplicationRecord
   validates :project_id, presence: true, numericality: true
   validates :material_id, presence: true, numericality: true
   validates :unit_price, presence: true, numericality: { greater_than_or_equal_to: 0, message: 'has to be 0 or greater' }
-  validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0, message: 'has to be 0 or greater' }, length: 10
+  validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0, message: 'has to be 0 or greater' }, length: { maximum: 10 }
 
   belongs_to :project
   belongs_to :material
