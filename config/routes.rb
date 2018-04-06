980fde49bd786d4622f51988c48d67b788393112
julius agno <agno.julius@gmail.com>
2018-04-05 23:09:43 -0500

    create admin_panel page

--- config/routes.rb
+++ config/routes.rb
@@ -60,6 +60,9 @@ Rails.application.routes.draw do
       match 'search' => 'assignments#search', via: [:get, :post], as: :search #for ransack
      end
   end
+  get "/pages/admin_panel" => "pages#admin_panel"
+
+  resources :pages
 
   root 'home#index'
 
