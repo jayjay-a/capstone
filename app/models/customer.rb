add6a47c0ca903d0b07afac1a79b1da001b5a964
Kyle <klockley@uh.edu>
2018-04-08 00:15:19 -0500

    Added phone and email validation for customer/ fixed migrate typo

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,10 +8,10 @@ class Customer < ApplicationRecord
   validates :customer_branch, allow_nil: true, length: { maximum: 50 }
   validates :customer_address_1, allow_nil: true, length: { maximum: 100 }
   validates :customer_address_2, allow_nil: true, length: { maximum: 100 }
-  validates :customer_rep_name_1, presence: true, length: { maximum: 70 }
-  validates :customer_rep_name_2, allow_nil: true, length: { maximum: 70 }
-  validates :customer_rep_phone_1, allow_nil: true, length: { maximum: 15 }, format: { with: /\A\(?\d{3}\)?[- ]?\d{3}[- ]?\d{4}\z/, message: 'needs to be in (XXX) XXX-XXX format' }
-  validates :customer_rep_phone_2, allow_nil: true, length: { maximum: 26 }, format: { with: /[(]\d{3}[)] \d{3}-\d{4}/, message: 'needs to be in (XXX) XXX-XXX format' }
+  validates :customer_rep_name_1, presence: true, length: { maximum: 70 }, format: { with: /\A(?=.* )[^0-9`!@#\$%\^&*+_=(){}]+\z/ }
+  validates :customer_rep_name_2, allow_nil: true, length: { maximum: 70 }, format: { with: /\A(?=.* )[^0-9`!@#\$%\^&*+_=(){}]+\z/ }
+  validates :customer_rep_phone_1, allow_nil: true, length: { maximum: 15 }, format: { with: /\A\(?\d{3}\)?[- ]?\d{3}[- ]?\d{4}\z/, message: 'needs to only be numbers and -\'s or ()\'s' }
+  validates :customer_rep_phone_2, allow_nil: true, length: { maximum: 26 }, format: { with: /\A\(?\d{3}\)?[- ]?\d{3}[- ]?\d{4}\z/, message: 'needs to only be numbers and -\'s or ()\'s' }
   validates :customer_rep_email_1, allow_nil: true, length: { maximum: 70 }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create, message: 'is not a valid email address' }
   validates :customer_rep_email_2, allow_nil: true, length: { maximum: 70 }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create, message: 'is not a valid email address' }
 
