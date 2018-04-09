430dbb9a1af39c418ea115ab7692125896383956
Kyle <klockley@uh.edu>
2018-04-09 12:48:01 -0500

    Fixed name validations to having spacing optional

--- app/models/job_note.rb
+++ app/models/job_note.rb
@@ -2,6 +2,6 @@ class JobNote < ApplicationRecord
   belongs_to :job
 
   validates :job_notes, presence: true, length: { maximum: 5000 }
-  validates :job_note_owner, presence: true, length: { maximum: 70 }, format: { with: /\A(?=.* )[^0-9`!@#\$%\^&*+_=(){}]+\z/ }
+  validates :job_note_owner, presence: true, length: { maximum: 70 }, format: { with: /\A[^0-9`!@#\$%\^&*+_=(){}]+(?: [a-zA-Z]+)?\z/ }
   validates :job_note_date, presence: true
 end
