eeb90f0cb5b1a585cd74c6c3ab6994eb4ed676ac
julius agno <agno.julius@gmail.com>
2018-04-10 16:09:43 -0500

    bid amount should be nullable

--- app/models/project.rb
+++ app/models/project.rb
@@ -20,7 +20,7 @@ class Project < ApplicationRecord
   validates :applicable_tax, allow_nil: true, numericality: { greater_than_or_equal_to: 0, message: 'has to be 0 or greater' }
   validates :bid_fuel_cost, allow_nil: true, numericality: { greater_than_or_equal_to: 0, message: 'has to be 0 or greater' }
   validates :bid_lodging_cost, allow_nil: true, numericality: { greater_than_or_equal_to: 0, message: 'has to be 0 or greater' }
-  validates :bid_amount, numericality: { greater_than_or_equal_to: 0, message: 'has to be 0 or greater' }
+  validates :bid_amount, allow_nil: true, numericality: { greater_than_or_equal_to: 0, message: 'has to be 0 or greater' }
   validate :project_start_date_cannot_be_before_bid_submit_date, unless: -> { project_start_date.blank? }
   validate :project_end_date_cannot_be_before_project_start_date, unless: -> { project_end_date.blank? }
 
