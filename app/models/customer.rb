5c7afef56bca1fb6e86c6dadb99976ef4554a461
Kyle <klockley@uh.edu>
2018-04-12 21:59:43 -0500

    Fixed Name formatting to allow some special characters

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,11 +8,11 @@ class Customer < ApplicationRecord
   validates :customer_address_1, allow_blank: true, length: { maximum: 100 }
   validates :customer_address_2, allow_blank: true, length: { maximum: 100 }
   validates :customer_rep_name_1, presence: true, length: { maximum: 70 }, format: { with: /\A[a-zA-Z'\-.\s]*\z/, message: 'can only be letters' }
-  validates :customer_rep_name_2, allow_blank: true, length: { maximum: 70 }, format: { with: /\A[a-zA-Z\s]*\z/, message: 'can only be letters' }
+  validates :customer_rep_name_2, allow_blank: true, length: { maximum: 70 }, format: { with: /\A[a-zA-Z'\-.\s]*\z/, message: 'can only be letters' }
   validates :customer_rep_phone_1, allow_blank: true, length: { maximum: 15 }, format: { with: /\A\(?\d{3}\)?[- ]?\d{3}[- ]?\d{4}\z/, message: 'needs to only be numbers and -\'s or ()\'s' }
   validates :customer_rep_email_1, allow_blank: true, length: { maximum: 70 }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create, message: 'is not a valid email address' }
   validates :customer_rep_email_2, allow_blank: true, length: { maximum: 70 }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create, message: 'is not a valid email address' }
-  validates :customer_city, allow_blank: true, length: { maximum: 64 }, format: { with: /\A[a-zA-Z.\d\s]*\z/, message: 'can only be letters' }
+  validates :customer_city, allow_blank: true, length: { maximum: 64 }, format: { with: /\A[a-zA-Z'\-.\s]*\z/, message: 'can only be letters' }
   validates_format_of :customer_zipcode, allow_blank: true, length: { maximum: 16 }, with: /\A\d{5}(-\d{4})?\z/, message: 'should be in the form 12345 or 12345-1234'
 
   def customer_and_branch
