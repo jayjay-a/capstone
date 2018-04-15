ca8467b325c55bee82b599bcc825651badc862f3
julius agno <agno.julius@gmail.com>
2018-04-14 22:20:56 -0500

    add validation for Name on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable, :rememberable, :trackable, :recoverable, 
   devise :database_authenticatable, :validatable
+
+  validates :name, presence: true
 end
