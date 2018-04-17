b94ff1bf501d4b6f8460792c9ff5b4eb7d567179
julius agno <agno.julius@gmail.com>
2018-04-17 00:46:05 -0500

    fix guest permissions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,8 +3,7 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :index, :all #set all indexes to redirect to login first
-      if user.present?
+      if user.present? # users can create, read, update from all except lookup and user tables. cannot delete any
         can :create, :all
         can :read, :all
         can :update, :all
@@ -22,7 +21,10 @@ class Ability
                          Employee, State]
         can :show, User, id: user.id
         can :update, User, id: user.id
-        
+        if user.email.blank? # cheap way of finding out of they're logged in
+          cannot :manage, :all
+          can :index, :all #indexes redirect them to login page
+        end
         if user.admin?
           can :manage, :all
           cannot :destroy, User, id: user.id
