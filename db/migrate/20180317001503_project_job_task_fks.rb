81b19809fe10a8043790ed214050ff6a91637286
julius agno <agno.julius@gmail.com>
2018-04-17 00:02:14 -0500

    handle fk through rails

--- db/migrate/20180317001503_project_job_task_fks.rb
+++
@@ -1,9 +0,0 @@
-class ProjectJobTaskFks < ActiveRecord::Migration[5.1]
-  def change
-    add_index :jobs, :project_id
-    add_foreign_key :jobs, :projects, primary_key: :project_id
-
-    add_index :tasks, :job_id
-    add_foreign_key :tasks, :jobs, primary_key: :job_id
-  end
-end
