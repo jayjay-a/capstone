83571ffdc5101272d7492258258cf85355252b0f
julius agno <agno.julius@gmail.com>
2018-04-18 00:33:35 -0500

    deleting job/tasks go to project

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -67,7 +67,7 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     respond_to do |format|
-      format.html { redirect_to tasks_url, notice: 'Task was successfully deleted.' }
+      format.html { redirect_to projects_url, notice: 'Task was successfully deleted.' }
       format.json { head :no_content }
     end
   end
